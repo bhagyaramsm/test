e797628b984840c3d3142a5ff031a35b6960bfcc
bhagyaramsm <59008556+bhagyaramsm@users.noreply.github.com>
2019-12-18 12:39:36 +0530

    Add files via upload

---
+++ dummy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<br><h1>Hello, World!</h1>'
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
